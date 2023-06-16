ef904d07c83c4865d49967c1e59c89ed98c84f69
jcechace <jcechace@gmail.com>
2023-06-16 13:04:59 +0200

    Cashing and loading strimzi operator image

--- create.sh
+++ create.sh
@@ -15,15 +15,17 @@ kubectl config set-context --current --namespace $NAMESPACE
 echo ">>> Pulling images"
 docker pull quay.io/debezium/example-mongodb:latest
 docker pull quay.io/debezium/operator:devconf
+docker pull quay.io/strimzi/operator:0.35.1
 docker pull quay.io/strimzi/kafka:0.35.1-kafka-3.4.0 
 docker pull quay.io/debezium/server:2.3
+
 
 echo ">>> Loading images"
 kind load docker-image quay.io/debezium/example-mongodb:latest --name $CLUSTER
 kind load docker-image quay.io/debezium/operator:devconf --name $CLUSTER
 kind load docker-image quay.io/debezium/server:2.3 --name $CLUSTER
 kind load docker-image quay.io/strimzi/kafka:0.35.1-kafka-3.4.0 --name $CLUSTER
-
+kind load docker-image quay.io/strimzi/operator:0.35.1 --name $CLUSTER
 
 # Deploy Operators
 echo ">>> Deploying Operators"
@@ -32,9 +34,9 @@ kubectl create -f infra/operators/debezium.yml -n $NAMESPACE
 
 # Wait for operators
 echo ">>> Waiting for operators"
-kubectl wait --for=condition=Available deployments/strimzi-cluster-operator  --timeout=120s -n $NAMESPACE
+kubectl wait --for=condition=Available deployments/strimzi-cluster-operator  --timeout=240s -n $NAMESPACE
 echo ">>> Strimzi operator ready"
-kubectl wait --for=condition=Available deployments/debezium-operator  --timeout=120s -n $NAMESPACE
+kubectl wait --for=condition=Available deployments/debezium-operator  --timeout=240s -n $NAMESPACE
 echo ">>> Debezium operator ready"
 
 
@@ -51,15 +53,15 @@ kubectl apply -f infra/kafka.yml -n $NAMESPACE
 echo ">>> Waiting for infrastructure resources"
 ## Mongo
 echo ">>> Waiting for MongoDB"
-kubectl wait --for=condition=Available deployments/mongo --timeout=120s -n $NAMESPACE
+kubectl wait --for=condition=Available deployments/mongo --timeout=240s -n $NAMESPACE
 echo ">>> Initializing MongoDB Replica Set"
 kubectl exec -t deployments/mongo -n $NAMESPACE  -- bash -c '/usr/local/bin/init-inventory.sh -h mongo' 
 echo ">>> MongoDB ready"
 ## Tooling
 echo ">>> Waiting for tooling"
-kubectl wait --for=condition=Ready pods/tooling --timeout=120s -n $NAMESPACE
+kubectl wait --for=condition=Ready pods/tooling --timeout=240s -n $NAMESPACE
 echo ">>> Tooling ready"
 ## Kafka
 echo ">>> Waiting  for Kafka"
-kubectl wait --for=condition=Ready kafkas/dbz-kafka --timeout=120s -n $NAMESPACE
+kubectl wait --for=condition=Ready kafkas/dbz-kafka --timeout=240s -n $NAMESPACE
 echo ">>> Kafka ready"
